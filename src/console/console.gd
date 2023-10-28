0504032ffee0a30bc41a5e121f5350cbb85a0f72
zeltop <xyz3lt@gmail.com>
2023-10-28 19:36:26 +0200

    console

---
+++ src/console/console.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_text_edit_text_changed():
+	var text_input = $Console/TextEdit
+	if text_input.text.ends_with("\n"):
+		ConsoleHandler.process_command(text_input.text.rstrip("\n"))
+		text_input.clear()
