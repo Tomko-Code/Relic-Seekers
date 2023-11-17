73103c1f988ab1d4b3021a292b0ced9784b703eb
Tomko-Code <tom.zaskorski@gmail.com>
2023-11-17 10:04:12 +0100

    change starting zone to swamp prolog 00, minor chngages to code and some collisions for pits

--- src/zone/zone.gd
+++ src/zone/zone.gd
@@ -3,11 +3,3 @@ extends Node
 enum ZoneType{STATIC, GENERATED, GENERATED_PRESET}
 @export var type: ZoneType
 @export var zone_name: String = "null"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
