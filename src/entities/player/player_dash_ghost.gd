c6e8660040326a0937ba827e1e9b362aeac57bca
zeltop <xyz3lt@gmail.com>
2023-12-03 11:42:53 +0100

    added_dash

---
+++ src/entities/player/player_dash_ghost.gd
@@ -0,0 +1,18 @@
+class_name PlayerDashGhost
+extends AnimatedSprite2D
+
+func freeze_frame(sprite_component: AnimatedSpriteComponent):
+	var animated_sprite: AnimatedSprite2D = sprite_component.get_child(0)
+	var frames = animated_sprite.sprite_frames
+	
+	sprite_frames = frames.duplicate()
+	
+	animation = animated_sprite.animation
+	set_frame_and_progress(animated_sprite.frame, 0)
+
+
+func _ready():
+	var tween = create_tween().bind_node(self).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.1)
+	tween.tween_callback(queue_free)
+	
--- src/entities/player/user_movement_component.gd
+++ src/entities/player/user_movement_component.gd
@@ -2,32 +2,72 @@ class_name UserMovementComponent
 extends MovementComponent
 
 var acceleration = 10
+var is_dashing = false
+var can_dash = true
+
+var dash_max_cooldown = 1
+var dash_current_cooldown = 0
+
+var dash_max_duration = 0.15
+var dash_current_duration = 0
+var last_direction
+
+signal dash_over
+
+@export var _AnimatedSpriteComponent: AnimatedSpriteComponent
 
 func _physics_process(delta):
 	direction = Vector2.ZERO
 	
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
-
+	if not can_dash:
+		dash_current_cooldown += delta
+		if dash_current_cooldown >= dash_max_cooldown:
+			dash_current_cooldown = 0
+			can_dash = true
+	
+	if is_dashing:
+		dash_current_duration += delta
+		if dash_current_duration >= dash_max_duration:
+			parent.velocity = Vector2.ZERO
+			dash_current_duration = 0
+			is_dashing = false
+			emit_signal("dash_over")
+		else:
+			if _AnimatedSpriteComponent:
+				var ghost = PlayerDashGhost.new()
+				ghost.freeze_frame(_AnimatedSpriteComponent)
+				ghost.position = parent.position
+				parent.get_parent().call_deferred("add_child", ghost)
+	
+	if not is_dashing:
+		if Input.is_action_pressed("move_left"):
+			direction.x -= 1
+		if Input.is_action_pressed("move_right"):
+			direction.x += 1
+		if Input.is_action_pressed("move_up"):
+			direction.y -= 1
+		if Input.is_action_pressed("move_down"):
+			direction.y += 1
+	else:
+		direction = last_direction
+	
+	last_direction = direction
+	
+	if Input.is_action_pressed("dash") and direction != Vector2.ZERO and can_dash:
+		can_dash = false
+		is_dashing = true
+		
 	
 	if direction == Vector2.ZERO:
 		is_idle = true
 	else: is_idle = false
 	
 	#parent.velocity = direction.normalized() * delta * speed
-	parent.velocity = parent.velocity.lerp(direction * speed, acceleration * delta)
+	parent.velocity = parent.velocity.lerp(direction.normalized() * speed * (3 if is_dashing else 1), acceleration * delta * (10 if is_dashing else 1))
 	
 	parent.move_and_slide()
 	#var collision = parent.move_and_collide(parent.velocity * delta)
 	#if collision:
 	#	parent.velocity = parent.velocity.slide(collision.get_normal())
 	#	collision = parent.move_and_collide(parent.velocity * delta)
-
-
 
