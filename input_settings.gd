0a376bedbe940369baec0ac2f7828647490327ae
tomko-code <108701315+Tomko-Code@users.noreply.github.com>
2023-12-07 16:39:38 +0100

    Menu upgrade (#33)
    * slower flash
    
    * Changes to menu, save system, input is saved as resource, improve main menu visuals, add key bindings, remove shift from keybinds on dash, options for vsync and fullscreen
    
    * add fps counter
    
    * fix commit

---
+++ input_settings.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name InputSettings
+
+@export var data = {
+	
+}
--- src/menus/back.gd
+++ src/menus/back.gd
@@ -1,6 +1,8 @@
 extends Button
 
-
+func _input(event):
+	if Input.is_action_just_pressed("back"):
+		_on_pressed()
 
 func _on_pressed():
 	$"../../../../MainMenu".show()
