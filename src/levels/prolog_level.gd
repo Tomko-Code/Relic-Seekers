6f0261932bb81e3f8dab5bd3667fe48fc6f88c11
Tomko-Code <tom.zaskorski@gmail.com>
2023-12-13 19:01:02 +0100

    levels

---
+++ src/levels/prolog_level.gd
@@ -0,0 +1,5 @@
+extends Level
+class_name PrologLevel
+
+func set_up(args:Array = []) -> void:
+	pass
---
+++ src/levels/start_level.gd
@@ -0,0 +1,5 @@
+extends Level
+class_name StartLevel
+
+func set_up(args:Array = []) -> void:
+	pass
