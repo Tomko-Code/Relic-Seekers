d0b45adcdf4a44e0c42c8c3dbc26d19814a7ada9
Tomko-Code <tom.zaskorski@gmail.com>
2023-12-15 15:16:16 +0100

    add combat level and 2 rooms for it

--- src/levels/combat_level.gd
+++ src/levels/combat_level.gd
@@ -4,27 +4,23 @@ class_name CombatLevel
 func set_up(args:Array = []) -> void:
 	create_level(Vector2(10,10))
 	
-	var room1:RoomData = RoomData.new().set_up("Start")
-	var room2:RoomData = RoomData.new().set_up("Normal1")
+	var main_combat_room:RoomData = RoomData.new().set_up("main_combat_room")
+	var left_combat_room:RoomData = RoomData.new().set_up("left_combat_room")
 	
-	place_room(room1, Vector2(0,1))
-	place_room(room2, Vector2(0,0))
-	
+	place_room(main_combat_room, Vector2(3,3))
+	place_room(left_combat_room, Vector2(2,4))
 	#print(room2.get_connection( room1.connection_arry[0].get_map_outside_cord(), -room1.connection_arry[0].direction))
-	#print(room1.get_connection( room2.connection_arry[0].get_map_outside_cord(), -room2.connection_arry[0].direction))
-	
+
 	# crates both way connection
-	room1.connect_room(room2, room1.connection_arry[0])
+	left_combat_room.connect_room(main_combat_room, left_combat_room.connection_arry[0])
 	
 	print_number_map()
 	
 	# Spawn map
-	default_room = spawn_room(room1)
-	spawn_room(room2)
-	
+	default_room = spawn_room(main_combat_room)
 	currnet_active_room = default_room
 	
+	spawn_room(left_combat_room)
+	
 	# Set Player
-	GameManager.player.position = get_cord_center_position(room1.cord)
-
-
+	GameManager.player.position = get_cord_center_position((main_combat_room.cord)+Vector2(0,1))
