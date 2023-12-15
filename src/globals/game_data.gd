36d8a5f53d50c054c3aa6f34dbbdfca9dffb08bb
Tomko-Code <tom.zaskorski@gmail.com>
2023-12-15 23:25:11 +0100

    add close dor logic

--- src/globals/game_data.gd
+++ src/globals/game_data.gd
@@ -122,9 +122,9 @@ var rooms_data = {
 		"res" : load("res://src/rooms/combat/left_combat_room.tscn"),
 		"connections" : [[Vector2(0,0), Vector2.RIGHT]],
 		"waves" : [
-			[[1, 1, ["test_mob_a"]]],
-			[[1, 1, ["test_mob_a"]]],
-			[[1, 1, ["test_mob_a"]],[1, 1, ["test_mob_a"]]]
+			[[1, 1, ["test_mob_a"]]]
+			#[[1, 1, ["test_mob_a"]]],
+			#[[1, 1, ["test_mob_a"]],[1, 1, ["test_mob_a"]]]
 		]
 	},
 	#######################################
