173de1087e6d77f773efe65149555c74d65c2546
Tomko-Code <tom.zaskorski@gmail.com>
2023-12-15 23:00:27 +0100

    enemy spawn logic

--- src/zone/room_connection.gd
+++ src/zone/room_connection.gd
@@ -7,6 +7,7 @@ func tp(body):
 	body.global_position += data.direction * 800
 	var active_level:Level = GameManager.loaded_scenes["Game"].active_level 
 	active_level.currnet_active_room = data.connected_room.spawned_room
+	active_level.currnet_active_room.on_player_enter()
 
 func _on_area_2d_body_entered(body):
 	GameManager.game_camera.play_slow()
