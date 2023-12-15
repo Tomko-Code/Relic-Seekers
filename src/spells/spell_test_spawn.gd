736bcc223b7c76eef9de585015e018ff9b983790
zeltop <xyz3lt@gmail.com>
2023-12-15 19:34:45 +0100

    combat_zone changes

--- src/spells/spell_test_spawn.gd
+++ src/spells/spell_test_spawn.gd
@@ -1,10 +1,11 @@
 class_name SpellTestSpawn
 extends Marker2D
 
+@export var spell_name: String
+
 func _ready():
-	var spell = SpellsHandler.create_spell("test_spell")
+	var spell = SpellsHandler.create_spell(spell_name)
 	var spell_pickup = SpellsHandler.create_spell_pickup(spell)
 	spell_pickup.position = position
 	get_parent().call_deferred("add_child", spell_pickup)
 	queue_free()
-	
