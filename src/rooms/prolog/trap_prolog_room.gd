1d7803f4aa96dc0c7f04061ac1753dba1e419b28
3fTL <114991498+3fTL@users.noreply.github.com>
2023-12-18 13:30:16 +0100

    Spike room (#59)
    * spike room
    
    * remove camera

---
+++ src/rooms/prolog/trap_prolog_room.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+
+var current_duration = 0.0
+var cycle_duration = 0.75
+
+enum states {
+	SPIKES_A,
+	TRANSIENT_A,
+	SPIKES_B,
+	TRANSIENT_B,
+}
+
+var state = states.SPIKES_A
+@onready var spikes_a = get_tree().get_nodes_in_group("spikes_a")
+@onready var spikes_b = get_tree().get_nodes_in_group("spikes_b")
+
+func _ready():
+	for spike in spikes_a:
+		spike.active = true
+
+func _process(delta):
+	current_duration += delta
+	if current_duration >= cycle_duration:
+		current_duration -= cycle_duration
+		match state:
+			states.SPIKES_A:
+				for spike in spikes_a:
+					spike.active = false
+				state = states.TRANSIENT_A
+			states.TRANSIENT_A:
+				for spike in spikes_b:
+					spike.active = true
+				state = states.SPIKES_B
+			states.SPIKES_B:
+				for spike in spikes_b:
+					spike.active = false
+				state = states.TRANSIENT_B
+			states.TRANSIENT_B:
+				for spike in spikes_a:
+					spike.active = true
+				state = states.SPIKES_A
