d72ca5d6b6faa309a5e2363714507074beec7cad
Tomko-Code <tom.zaskorski@gmail.com>
2025-05-28 22:13:57 +0200

    added footsteps

---
+++ src/entities/player/footsteps.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer
+
+func _process(delta: float) -> void:
+	if not get_parent().is_idle:
+		if not playing:
+			play()
+			pitch_scale = 1 + randf_range(-0.15, 0.15)
+	else:
+		stop()
