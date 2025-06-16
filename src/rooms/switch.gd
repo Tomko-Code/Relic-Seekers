962cb089c61d0b6feb6051a3792cf9733f344bcc
Tomko-Code <tom.zaskorski@gmail.com>
2025-06-16 10:52:07 +0200

    leaver sound and sprite

--- src/rooms/switch.gd
+++ src/rooms/switch.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+func _ready() -> void:
+	if !GameData.save_file.portal_room_open:
+		$AnimatedSprite2D.play("close")
+	else:
+		$AnimatedSprite2D.play("open")
+
 func _on_interactable_component_interacted() -> void:
 	if GameData.save_file.portal_room_open:
 		get_tree().call_group("sanctuary_door", "on_closed")
+		$AnimatedSprite2D.play("close")
+		$AudioStreamPlayer.play()
 	else:
 		get_tree().call_group("sanctuary_door", "on_opend")
+		$AnimatedSprite2D.play("open")
+		$AudioStreamPlayer.play()
